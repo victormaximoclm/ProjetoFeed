c5fc2b7f5a036b6e48cdb81547524e550e273137
VICTOR MAXIMO COSTA LIMA DE <victor.maximo@dnacenter>
2023-10-07 10:38:30 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 //JSX = JavaScript + XML
 import {Post} from './Post';
+import { Header } from './components/header';
+
+import './styles.css';
 
 export function App() {
   return (
    <div>
+    <Header />
+
     <Post 
     author="Victor Maximo" 
     content="Loren Ipsun....." 
